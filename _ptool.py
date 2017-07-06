7394f0d0f037c551297aa004c15444f79571f634
Laiyyi <ericxji08025etdoop@gmail.com>
2017-07-06 22:40:26 +0800

    new get back
    there is a little problbm

--- _ptool.py
+++ _ptool.py
@@ -12,28 +12,43 @@ def get_web_page(url):
     else:
        return resp.text
 
-def get_articles(html,date):
+def get_back(html):
+    ptt = 'https://www.ptt.cc/'
+    doc = BeautifulSoup(html,'html.parser')
+    divs = doc.find_all('div','btn-group btn-group-paging')
+
+    for d in divs:
+            #if d.find('a','btn wide disabled') is not None:
+              # if d.find('a','btn wide disabled').string=='‹ 上頁':
+                 # ptt = 'https://www.ptt.cc/bbs/Gossiping/index1.html'
+                  #break
+               #else:
+                  ptt= ptt+ d.find('a','btn wide',1,'‹ 上頁')['href']
+       
+    return ptt
+
+def get_articles(html):
     soup = BeautifulSoup(html,'html.parser')
     
     articles = [] #save articles
     divs = soup.find_all('div','r-ent')
     for d in divs:
-        #if d.find('div','date').string == date:
-          #push_count
-           push = 0
+           push = 0 #push_count
+           
+           #get_push_count
            if d.find('div','nrec').string:
               try:
                   push = int(d.find('div','nrec').string)
               except ValueError:
                   pass
           
-          #get
-           if d.find('a'):
+          #get_articles
+           if d.find('a'):                #if article exists
               href = d.find('a')['href']
               title = d.find('a').string
               articles.append({
-                  'title':title,
-                  'href':href,
-                  'push':push
+                   'title':title,
+                   'href':href,
+                   'push':push
               })
     return articles
--- _ptt.py
+++ _ptt.py
@@ -1,10 +1,17 @@
 import _ptool
-import time
 
-page = _ptool.get_web_page('https://www.ptt.cc/bbs/Gossiping/index.html')
+url='https://www.ptt.cc/bbs/Gossiping/index.html'
+page = _ptool.get_web_page(url)
 
-if page:
-    date = time.strftime("%m/%d").lstrip('0')
-    cu = _ptool.get_articles(page,date)
+for x in range(0,5):
+    cu = []
+    if x == 0 :
+       cu = _ptool.get_articles(page)
+   
+    else:
+       url = _ptool.get_back(page)
+       page =_ptool.get_web_page(url)
+       cu = _ptool.get_articles(page)
+
     for post in cu:
-        print(post)
+         print(post)

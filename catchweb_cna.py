7d2e9ed6fc995efd651fa6f30e7595fcf72390a1
brendatsai011220 <brendatsai011220@gmail.com>
2017-07-07 01:22:59 -0700

    Update and rename nextpage.py to catchweb_cna.py
    can catch cna web 
    $ python3 catchweb_cna.py [keyword] >[keyword.txt]

--- catchweb_cna.py
+++ catchweb_cna.py
@@ -10,50 +10,54 @@ import lxml
 #sys.setdefaultencoding('utf-8')
 
 from urllib.request import urlopen
+from urllib.parse import quote
 import datetime
 import re
+import sys
 
 #GET<Response [200]>
 res = requests.get("http://www.cna.com.tw/search/hysearchws.aspx?q=%E5%90%8C%E5%A9%9A")
 
-#經過BeautifulSoup內lxml編輯器解析的結果
-soup = BeautifulSoup(res.text,'lxml')
-
 #initial set
-key = "%E5%90%8C%E5%A9%9A"
-nowWeb = '/search/hysearchws.aspx?q='+ key
+#key = "%E5%90%8C%E5%A9%9A"
+key = sys.argv[1]
+key = quote(key, safe='/:?=&')
+print(key)
+nowWeb = 'http://www.cna.com.tw/search/hysearchws.aspx?q='+ key
 
 #印出網頁內容
 print (nowWeb)
  
 def getSearchPages(searchUrl):   
- page_html = urlopen("http://www.cna.com.tw"+searchUrl)
+ page_html = urlopen(searchUrl)
  page_bsObj = BeautifulSoup(page_html,'lxml')
- return page_bsObj.find("div", {"class":"pagination"},"ul").findAll("a",href=re.compile("^(http://www.cna.com.tw/search/)(.)*$"))
-
+ result = page_bsObj.find("div", {"class":"pagination"},"ul").findAll("a",href=re.compile("^(http://www.cna.com.tw/search/)(.)*$"))
+ return result
 
 
 def getLinks(articleUrl):   
- html = urlopen("http://www.cna.com.tw"+articleUrl)
+ html = urlopen(articleUrl)
  bsObj = BeautifulSoup(html,'lxml')
  print("---------------------------------")
  #print(bsObj)
  return bsObj.find("div", {"class":"search_result_list"},"ul").findAll("a",href=re.compile("^(/news/)((?!:).)*$"))
   
-searchlinks = getSearchPages(nowWeb)
-print("search links")
-print(searchlinks)
+#searchlinks = getSearchPages(nowWeb)
+#print("search links")
+#print(searchlinks)
 pages = set()
 pages.add(nowWeb) 
 searchIdx=1
-for searchIdx in range(1,len(searchlinks)-1,1):
-#while len(links) > 0:
+
+while True:
+
  global pages
  #get new article
- 
+
  links = getLinks(nowWeb)
  j=0
- for j in range(0,len(links)-1,1):
+  
+ for j in range(0,len(links),1):
   newArticle = links[j].attrs["href"]	 
   print(newArticle)
   pages.add(newArticle)
@@ -66,16 +70,38 @@ for searchIdx in range(1,len(searchlinks)-1,1):
    context = newpageSoup.select('p')[i].text
    print(context)
   links = getLinks(nowWeb)
- 
+  
  #change new search page
+ if searchIdx == 1: #p1 -> p11 search only at first page
+  searchlinks = getSearchPages(nowWeb)
+ if searchlinks == -1:
+  break
+ print(nowWeb)
  newSearchPage = searchlinks[searchIdx].attrs["href"]
- print("*************************** next page *********************************")
+ #print("*************************** next page *********************************")
  print(newSearchPage)
- pages.add(newSearchPage)
  
+ 
+  
+ #change chinese to url
+ newSearchPage = quote(newSearchPage, safe='/:?=&')
+ print(newSearchPage) 
  newSearchPage_html = urlopen(newSearchPage)
  newSearchPage_Soup = BeautifulSoup(newSearchPage_html,'lxml')
  nowWeb = newSearchPage
+ print(nowWeb)
  
+ if newSearchPage not in pages:
+  pages.add(newSearchPage)
+  searchIdx =searchIdx + 1
+
+ else:
+  break
+  
+ if searchIdx >= len(searchlinks):
+  searchlinks = getSearchPages(nowWeb)
+  searchIdx = 2
+  print(searchIdx)
+
  
 #print(links)

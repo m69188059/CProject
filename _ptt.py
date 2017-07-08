b00274aa57c776ad1d17b44ab180752ee81120a8
Laiyyi <ericxji08025etdoop@gmail.com>
2017-07-08 19:32:07 +0800

    slow work

--- _ptt.py
+++ _ptt.py
@@ -1,17 +1,22 @@
 import _ptool
+#import re
 
 url='https://www.ptt.cc/bbs/Gossiping/index.html'
-page = _ptool.get_web_page(url)
+page = _ptool.get_web(url)
+keyword = ''
+_isfirst = 1
 
-for x in range(0,5):
-    cu = []
-    if x == 0 :
-       cu = _ptool.get_articles(page)
-   
+
+while True:
+    web = []
+    if _isfirst:
+       _isfirst = 0
     else:
        url = _ptool.get_back(page)
-       page =_ptool.get_web_page(url)
-       cu = _ptool.get_articles(page)
+       if url is '1':
+          break;
+       page = _ptool.get_web(url)
+    web = _ptool.get_articles(page,keyword)
+    for post in web:
+        print(post['title'])
 
-    for post in cu:
-         print(post)

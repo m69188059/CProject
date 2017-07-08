47f44385a70985d6eab2650d3f8640254c0d2de3
Laiyyi <ericxji08025etdoop@gmail.com>
2017-07-08 20:13:46 +0800

    Add files via upload

--- _ptool.py
+++ _ptool.py
@@ -1,11 +1,9 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
-def get_web_page(url):
-    resp = requests.get(
-        url=url,
-        cookies={'over18':'1'}
-    )
+def get_web(url):
+    resp = requests.get( url=url, cookies={'over18':'1'})      #for 18 verification
     if resp.status_code !=200:
        print('Invalid url:',resp.url)
        return None
@@ -13,42 +11,57 @@ def get_web_page(url):
        return resp.text
 
 def get_back(html):
-    ptt = 'https://www.ptt.cc/'
     doc = BeautifulSoup(html,'html.parser')
     divs = doc.find_all('div','btn-group btn-group-paging')
-
-    for d in divs:
-            #if d.find('a','btn wide disabled') is not None:
-              # if d.find('a','btn wide disabled').string=='‹ 上頁':
-                 # ptt = 'https://www.ptt.cc/bbs/Gossiping/index1.html'
-                  #break
-               #else:
-                  ptt= ptt+ d.find('a','btn wide',1,'‹ 上頁')['href']
-       
-    return ptt
-
-def get_articles(html):
-    soup = BeautifulSoup(html,'html.parser')
     
-    articles = [] #save articles
-    divs = soup.find_all('div','r-ent')
+    ptt_url = 'https://www.ptt.cc/'
+    done = '0'
     for d in divs:
-           push = 0 #push_count
+        if d.find('a','btn wide','1','‹ 上頁'):
+           ptt_url= ptt_url+ d.find('a','btn wide',1,'‹ 上頁')['href']
+        else:
+           done = '1'
+           break
+    
+    if done is '1':
+         return done
+    return ptt_url
+
+def get_articles(html,search_key):
+    doc = BeautifulSoup(html,'html.parser')
+    divs = doc.find_all('div','r-ent')
+    
+    ptt = 'https://www.ptt.cc/'   
+    articles = []                       
+    pattern = search_key + '+'                       #set keyword
+
+    for d in divs:
+           push = 0                             
            
-           #get_push_count
+           #=== get_push_count ===
+
+
            if d.find('div','nrec').string:
               try:
                   push = int(d.find('div','nrec').string)
               except ValueError:
                   pass
-          
-          #get_articles
-           if d.find('a'):                #if article exists
-              href = d.find('a')['href']
+
+          #=== get_articles ===
+           if d.find('a'):                              #article exists
               title = d.find('a').string
-              articles.append({
-                   'title':title,
-                   'href':href,
-                   'push':push
-              })
+              
+              if re.findall(pattern,title):             #article matches keyword
+                 href = d.find('a')['href']
+                 articles.append({
+                    'title':title,
+                    'link':ptt+href,
+                    'push':push })
     return articles
+
+def get_in_article(html):
+    soup = BeautifulSoup(html,'html.parser')
+    divs = soup.find_all('div','article-metaline')
+    #put time parameter
+    #for d in divs:
+        #print(d.find('span','article-meta-value',3))
--- _ptt.py
+++ _ptt.py
@@ -1,17 +1,21 @@
 import _ptool
 
-url='https://www.ptt.cc/bbs/Gossiping/index.html'
-page = _ptool.get_web_page(url)
+url='https://www.ptt.cc/bbs/Gossiping/index100.html'
+page = _ptool.get_web(url)
+keyword = '八卦'
+_isfirst = 1
 
-for x in range(0,5):
-    cu = []
-    if x == 0 :
-       cu = _ptool.get_articles(page)
-   
+
+while True:
+    web = []
+    if _isfirst:
+       _isfirst = 0
     else:
        url = _ptool.get_back(page)
-       page =_ptool.get_web_page(url)
-       cu = _ptool.get_articles(page)
+       if url is '1':
+          break;
+       page = _ptool.get_web(url)
+    web = _ptool.get_articles(page,keyword)
+    for post in web:
+        print(post['title'])
 
-    for post in cu:
-         print(post)

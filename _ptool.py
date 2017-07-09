af2ea2aa5d711a60e8f84daf7af79637aef27686
Laiyyi <ericxji08025etdoop@gmail.com>
2017-07-09 17:17:49 +0800

    PTT

--- _ptool.py
+++ _ptool.py
@@ -1,5 +1,4 @@
 import requests
-import re
 from bs4 import BeautifulSoup
 
 def get_web(url):
@@ -10,8 +9,11 @@ def get_web(url):
     else:
        return resp.text
 
-def get_back(html):
-    doc = BeautifulSoup(html,'html.parser')
+def get_doc(page):
+    page = BeautifulSoup(page,'html.parser')
+    return page
+
+def get_back(doc):
     divs = doc.find_all('div','btn-group btn-group-paging')
     
     ptt_url = 'https://www.ptt.cc/'
@@ -27,14 +29,12 @@ def get_back(html):
          return done
     return ptt_url
 
-def get_articles(html,search_key):
-    doc = BeautifulSoup(html,'html.parser')
+def get_articles(search_key,doc):
     divs = doc.find_all('div','r-ent')
     
     ptt = 'https://www.ptt.cc/'   
     articles = []                       
-    pattern = search_key + '+'                       #set keyword
-
+    
     for d in divs:
            push = 0                             
            
@@ -48,20 +48,27 @@ def get_articles(html,search_key):
                   pass
 
           #=== get_articles ===
-           if d.find('a'):                              #article exists
-              title = d.find('a').string
-              
-              if re.findall(pattern,title):             #article matches keyword
-                 href = d.find('a')['href']
-                 articles.append({
-                    'title':title,
-                    'link':ptt+href,
-                    'push':push })
+           if d.find('a'):                         #article exists
+              title = d.find('a').string                  
+            
+              try:
+                title.find(search_key)                
+              except AttributeError as e:
+                return None
+              else:
+                if title.find(search_key) is not -1:
+                   href = d.find('a')['href']
+                   articles.append({
+                       'title':title,
+                       'link':ptt+href,
+                       'push':push })
+             
     return articles
 
-def get_in_article(html):
-    soup = BeautifulSoup(html,'html.parser')
-    divs = soup.find_all('div','article-metaline')
-    #put time parameter
-    #for d in divs:
-        #print(d.find('span','article-meta-value',3))
+def get_in_article(doc):
+    divs = doc.find_all('div','article-metaline')
+    
+    for d in divs:
+        st = d.find('span','article-meta-tag').string + ':' + d.find('span','article-meta-value').string
+        print(st)
+       
--- _ptt.py
+++ _ptt.py
@@ -1,21 +1,40 @@
 import _ptool
+import time
 
-url='https://www.ptt.cc/bbs/Gossiping/index100.html'
+url='https://www.ptt.cc/bbs/Gossiping/index.html'
 page = _ptool.get_web(url)
-keyword = '八卦'
+keyword = '新聞'
 _isfirst = 1
 
+#r = 1
+#start=time.time()
 
 while True:
     web = []
+
+ #   print(r)
+ #   r=r+1
+
+    html=_ptool.get_doc(page)
+    
     if _isfirst:
        _isfirst = 0
     else:
-       url = _ptool.get_back(page)
+       url = _ptool.get_back(html)
        if url is '1':
           break;
        page = _ptool.get_web(url)
-    web = _ptool.get_articles(page,keyword)
-    for post in web:
-        print(post['title'])
 
+    web = _ptool.get_articles(keyword,html)
+    try:
+        for post in web:
+          #  print(post['title'])
+            page_1 =  _ptool.get_web(post['link'])
+            html_1 = _ptool.get_doc(page_1)
+            _ptool.get_in_article(html_1)
+    except TypeError as e:
+        pass
+   
+        
+#tnd=time.time()
+#print(tnd-start)
